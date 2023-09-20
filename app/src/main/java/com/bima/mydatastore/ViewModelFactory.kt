3b39188dbaf5fe799d3ee52b163c95cb74900f71
bimamib <bimaprast1601@gmail.com>
2023-09-20 17:22:12 +0700

    fixing bug

--- app/src/main/java/com/bima/mydatastore/ViewModelFactory.kt
+++ app/src/main/java/com/bima/mydatastore/ViewModelFactory.kt
@@ -1,9 +1,9 @@
 package com.bima.mydatastore
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.NewInstanceFactory
 
-class ViewModelFactory(private val pref: SettingPreferences) : ViewModelProvider.NewInstanceFactory() {
+class ViewModelFactory(private val pref: SettingPreferences) : NewInstanceFactory() {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
